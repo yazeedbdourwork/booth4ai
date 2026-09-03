7603964df5bb5f2eb71748bd2a8f8fb735374436
yazeedbdourwork <yazeedbdourwork@gmail.com>
2026-09-04 01:21:51 +0300

    Update supabase.ts

--- booth4ai/lib/supabase.ts
+++ booth4ai/lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://msqcwcfehflyyhgztsxg.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_N7OMoGL7bE5Y1zUcj0Z26A_6A5a_wUp";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  ".supahttps://msqcwcfehflyyhgztsxgbase.co";
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "sb_publishable_N7OMoGL7bE5Y1zUcj0Z26A_6A5a_wUp"; // Paste your actual anon JWT key here
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
