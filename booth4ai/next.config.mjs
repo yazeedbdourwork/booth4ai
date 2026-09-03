a3e9a777a242a602f7f906fb9a946d94aeb228a5
yazeedbdourwork <yazeedbdourwork@gmail.com>
2026-09-03 23:16:27 +0300

    Add files via upload

---
+++ booth4ai/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
