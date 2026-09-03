cd0db0e8c0f4554c7d73ea5dd46303ab56867f8d
yazeedbdourwork <yazeedbdourwork@gmail.com>
2026-09-04 01:22:38 +0300

    Update supabase.ts

--- booth4ai/lib/supabase.ts
+++ booth4ai/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  ".supahttps://msqcwcfehflyyhgztsxgbase.co";
+  "https://msqcwcfehflyyhgztsxg.supabase.co";
 
 const supabaseAnonKey =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
