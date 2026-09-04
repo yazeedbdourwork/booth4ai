88cc79f21c05f3145d3a7571a2c3a21ad9a2da74
yazeedbdourwork <yazeedbdourwork@gmail.com>
2026-09-04 10:50:58 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  'https://msqcwcfehflyyhgztsxg.supabase.co',
-  'sb_publishable_N7OMoGL7bE5Y1zUcj0Z26A_6A5a_wUp'
-);
+const supabaseUrl = 'https://msqcwcfehflyyhgztsxg.supabase.co';
+const supabaseAnonKey = 'sb_publishable_N7OMoGL7bE5Y1zUcj0Z26A_6A5a_wUp';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
