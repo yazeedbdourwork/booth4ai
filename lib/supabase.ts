effd82c1929bf1f104eb8457c1792b38aa0d2eab
yazeedbdourwork <yazeedbdourwork@gmail.com>
2026-09-04 11:11:13 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,3 +4,5 @@ const supabaseUrl = 'https://msqcwcfehflyyhgztsxg.supabase.co';
 const supabaseAnonKey = 'sb_publishable_N7OMoGL7bE5Y1zUcj0Z26A_6A5a_wUp';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+process.env.NEXT_PUBLIC_SUPABASE_URL
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
